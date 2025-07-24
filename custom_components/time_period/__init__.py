55ba620a842a32a926ea477a4ce0ed65a7fc2a05
无为智能 <wuweizhineng@163.com>
2025-07-25 00:13:24 +0800

    Update __init__.py

--- custom_components/time_period/__init__.py
+++ custom_components/time_period/__init__.py
@@ -9,11 +9,9 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up integration from UI."""
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    return await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
